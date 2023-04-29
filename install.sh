85981c4221d54bac5d8195e7c7a950293c197fb3
keagud <keagud@protonmail.com>
2023-04-29 15:10:46 -0700

    fix shell script issue when bash is not the default login shell

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 VENV_TMP="/tmp/newsrc_venv/"
 python3 -m venv $VENV_TMP
 
@@ -10,7 +12,8 @@ $VENV_TMP/bin/shiv -c newsrc $( cat requirements.txt ) -o ~/.local/bin/newsrc  .
 
 TEMPLATE_DIR="$HOME/.local/templates/newsrc"
 
-if [[ ! -d  $TEMPLATE_DIR ]]; then
+if [[ ! -d  $TEMPLATE_DIR ]] 
+then
     mkdir -p $TEMPLATE_DIR
     cp ./templates/* $TEMPLATE_DIR/
 fi
--- templates/template.java
+++ templates/template.java
@@ -1,4 +1,4 @@
-import java.io.*
+import java.io.*;
 
 /** 
  *<h1> {{ name.title() }} </h1>
@@ -9,7 +9,7 @@ public class {{ name.title() }} {
 
   public static void main(String[] args){
 
-    System.out.println("Hello from {{ name }}")
+    System.out.println("Hello from {{ name }}");
 
 
   }
