318b046676091abd95e9a3b9329873310ac1c81a
keagud <keagud@protonmail.com>
2023-04-29 15:01:29 -0700

    add java template

---
+++ templates/template.java
@@ -0,0 +1,17 @@
+import java.io.*
+
+/** 
+ *<h1> {{ name.title() }} </h1>
+ * @since {{ date.isoformat() }} 
+ */
+
+public class {{ name.title() }} {
+
+  public static void main(String[] args){
+
+    System.out.println("Hello from {{ name }}")
+
+
+  }
+
+}
